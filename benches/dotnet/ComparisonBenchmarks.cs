68710b3950d1369b4a59990860d48f05f37d9182
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-06-06 21:33:09 +0200

    add `dotnet` benchmarks

---
+++ benches/dotnet/ComparisonBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using forsen;
+
+namespace libs_comparison;
+[MemoryDiagnoser(true)]
+public class ComparisonBenchmarks
+{
+    private readonly List<ReadOnlyMemory<byte>> dataLines = new();
+    private readonly List<string> stringLines = new();
+
+    [GlobalSetup]
+    public void AddData()
+    {
+        string[] lines = File.ReadLines("data.txt").Take(1000).ToArray();
+
+        foreach (string line in lines)
+        {
+            stringLines.Add(line);
+            dataLines.Add(Encoding.UTF8.GetBytes(line));
+        }
+
+        Console.WriteLine($"Added {dataLines.Count} lines");
+    }
+
+    [Benchmark]
+    public void MiniTwitchParse()
+    {
+        foreach (ReadOnlyMemory<byte> item in dataLines)
+        {
+            MiniTwitch.Process(item);
+        }
+    }
+
+    /* [Benchmark]
+    public void TwitchLibParse()
+    {
+        foreach (string item in stringLines)
+        {
+            TwitchLib.HandleIrcMessage(TwitchLib.ParseIrcMessage(item));
+        }
+    } */
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        Console.WriteLine($"Clearing {dataLines.Count} lines");
+        dataLines.Clear();
+        stringLines.Clear();
+    }
+}
---
+++ benches/dotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using libs_comparison;
+
+
+ManualConfig config = DefaultConfig.Instance
+    .AddJob(Job
+         .MediumRun
+         .WithLaunchCount(1)
+         .WithWarmupCount(5)
+         .WithIterationCount(5)
+         .WithToolchain(InProcessNoEmitToolchain.Instance));
+BenchmarkRunner.Run<ComparisonBenchmarks>(config);
