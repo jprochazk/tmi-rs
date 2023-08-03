843148a817a135db904d462f72a6a7a23378ba06
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-08-03 22:31:13 +0200

    add comments explaining simd impls

--- src/simd/arm_neon.rs
+++ src/simd/arm_neon.rs
@@ -1,3 +1,12 @@
+//! This implementation works exactly like the `x86_sse` one,
+//! but the method of finding the occurence and position of characters
+//! is different because of the lack of a `movemask` equivalent in NEON.
+//!
+//! The method used is explained in the following article (the link goes to a table of operations):
+//! https://community.arm.com/arm-community-blogs/b/infrastructure-solutions-blog/posts/porting-x86-vector-bitmask-optimizations-to-arm-neon#:~:text=Consider%20the%C2%A0result%20in%20both%20cases%20as%20the%20result%20of%20PMOVMSKB%20or%20shrn
+//!
+//! Archived link: https://web.archive.org/web/20230603011837/https://community.arm.com/arm-community-blogs/b/infrastructure-solutions-blog/posts/porting-x86-vector-bitmask-optimizations-to-arm-neon
+
 use crate::{leak, ParsedTags, Tags, Whitelist};
 
 use core::arch::aarch64 as simd;
@@ -5,8 +14,16 @@ use core::mem;
 use simd::uint8x16_t;
 use std::ops::Add;
 
+/// We don't have a SIMD implementation of `parse_prefix` in NEON,
+/// because it was not faster. Instead just re-export the scalar impl.
 pub use crate::scalar::parse_prefix;
 
+/// Parse IRC message tags:
+///
+/// `@key=value;other=etc `
+///
+/// Tags consist of semicolon-separated key-value pairs.
+/// The tag list is terminated by a ` ` character.
 pub fn parse_tags<'src, const IC: usize, F>(
   remainder: &'src str,
   whitelist: &Whitelist<IC, F>,
@@ -14,6 +31,10 @@ pub fn parse_tags<'src, const IC: usize, F>(
 where
   F: for<'a> Fn(&'a mut Tags<'static>, &'static str, &'static str),
 {
+  // This code is identical to the `x86_sse` version.
+  // It should not be duplicated, but seeing as there are only two SIMD implementations,
+  // I believe it is simpler to just copy the implementation, at least for now.
+
   if let Some(remainder) = remainder.strip_prefix('@') {
     let mut tags = Tags::with_capacity(IC);
 
@@ -63,6 +84,11 @@ where
   T: Add<usize, Output = T>,
   F: Fn(uint8x16_t) -> Option<T>,
 {
+  // This code is almost the same as `x86_sse`.
+  // The only difference is that NEON does not have alignment requirements
+  // for 8x16 vectors, so we use `vld1q_u8` in both the 16-byte chunk loop,
+  // and for any trailing characters.
+
   let bytes = s.as_bytes();
 
   let mut i = 0usize;
@@ -75,7 +101,8 @@ where
   }
   if i < bytes.len() {
     let mut buf = [0; 16];
-    buf[..bytes.len() - i].copy_from_slice(&bytes[i..]);
+    buf[..bytes.len() - i].copy_from_slice(&bytes[i..]); // memcpy
+
     let data = unsafe { simd::vld1q_u8(buf.as_ptr() as *const _) };
     if let Some(pos) = test(data) {
       return Some(pos + i);
@@ -85,11 +112,38 @@ where
   None
 }
 
+/// Find the first `=` character in `s`.
+///
+/// The implementation splits `s` into 16-byte chunks, loading each chunk into a single 8x16 vector.
+///
+/// The resulting 8x16 vectors are compared against the pre-filled vector of a single character using `vceqq_u8`.
+/// Next, the 8x16 is reinterpreted as 16x8, to which we apply `vshrn_n_u16`.
+///
+/// `vshrn_n_u16` performs a "vector shift right by constant and narrow".
+/// The way I understand it is that for every 16-bit element in the vector,
+/// it "snips off" the 4 most significant bits + 4 least significant bits:
+///
+/// ```text,ignore
+/// # for a single element:
+/// 1111111100000000 -> shift right by 4
+/// 0000111111110000 -> narrow to u8
+///         11110000
+/// ```
+///
+/// If we count the number of bits in the vector before the first bit set to `1`,
+/// then divide that number by `4`, we get the same result as a `movemask + ctz` would give us.
+///
+/// So the last step is to reinterpret the resulting 8x8 vector as a single 64-bit integer,
+/// which is our mask.
+/// Just like before, we can check for the presence of the "needle" by comparing the mask
+/// against `0`.
+/// To obtain the position of the charater, divide its trailing zeros by 4.
 #[inline(always)]
 fn find_equals(s: &str) -> Option<usize> {
   #[inline(always)]
   fn test(data: uint8x16_t) -> Option<usize> {
     const EQUALS: uint8x16_t = unsafe { mem::transmute([b'='; 16]) };
+
     let mask = unsafe { Mask::eq(data, EQUALS) };
     if mask.has_match() {
       Some(mask.first_match_index())
@@ -129,6 +183,12 @@ impl Add<Found> for usize {
   }
 }
 
+/// Find the first `;` or ` ` character in `s`.
+///
+/// If both are present in `s`, the one earlier one will be returned.
+///
+/// This works exactly like `find_equals`, but performs two comparisons at a time
+/// in separate vectors, one for `;` and one for ` `.
 #[inline(always)]
 fn find_semi_or_space(s: &str) -> Option<Found> {
   #[inline(always)]
@@ -161,21 +221,25 @@ fn find_semi_or_space(s: &str) -> Option<Found> {
 struct Mask(u64);
 
 impl Mask {
+  /// Compare `a` to `b`, and produce a mask similar to the one produced by `movemask`,
+  /// but with 4 bits set per character instead of 1.
   #[inline(always)]
   unsafe fn eq(a: uint8x16_t, b: uint8x16_t) -> Self {
     let mask = simd::vreinterpretq_u16_u8(simd::vceqq_u8(a, b));
-    let res = simd::vshrn_n_u16(mask, 4);
+    let res = simd::vshrn_n_u16(mask, 4); // the magic sauce
     let matches = simd::vget_lane_u64(simd::vreinterpret_u64_u8(res), 0);
     Mask(matches)
   }
 
   #[inline(always)]
   fn has_match(&self) -> bool {
+    // We have a match if the mask is not empty.
     self.0 != 0
   }
 
   #[inline(always)]
   fn first_match_index(&self) -> usize {
+    // There are 4 bits per character, so divide the trailing zeros by 4 (shift right by 2).
     (self.0.trailing_zeros() >> 2) as usize
   }
 }
--- src/simd/x86_sse.rs
+++ src/simd/x86_sse.rs
@@ -5,6 +5,12 @@ use core::mem;
 use simd::__m128i;
 use std::ops::Add;
 
+/// Parse IRC message tags:
+///
+/// `@key=value;other=etc `
+///
+/// Tags consist of semicolon-separated key-value pairs.
+/// The tag list is terminated by a ` ` character.
 pub fn parse_tags<'src, const IC: usize, F>(
   remainder: &'src str,
   whitelist: &Whitelist<IC, F>,
@@ -13,32 +19,50 @@ where
   F: for<'a> Fn(&'a mut Tags<'static>, &'static str, &'static str),
 {
   if let Some(remainder) = remainder.strip_prefix('@') {
+    // pre-allocate space for the tags
+    // this uses a configurable default `IC`, which stands for `Initial Capacity`.
+    // the library supports "whitelisting" tags, in which case we know the total
+    // capacity we will ever need and can pre-allocate it.
+    // in case we don't have a whitelist, then this will allocate 16 slots.
     let mut tags = Tags::with_capacity(IC);
 
     let mut remainder = remainder;
     while !remainder.is_empty() {
       let Some(key_end) = find_equals(remainder) else {
+        // exit the loop if we don't find a key. this only happens if the input is malformed.
         break;
       };
+      // `key_end` is inclusive, meaning `remainder[key_end] == '='`.
+      // value starts after the `=` character.
       let value_start = key_end + 1;
+      // value ends at `;` or ` ` character.
       let value_end = find_semi_or_space(unsafe { remainder.get_unchecked(value_start..) })
+        // we only search from `value_start`, so we offset the found position by it
         .map(|v| value_start + v);
 
       match value_end {
+        // if we found a semicolon, then insert the tag into the buffer,
+        // and attempt to find another tag.
         Some(Found::Semi(value_end)) => {
           let key = unsafe { leak(remainder.get_unchecked(..key_end)) };
           let value = unsafe { leak(remainder.get_unchecked(value_start..value_end)) };
           whitelist.maybe_insert(&mut tags, key, value);
+          // advance remainder to after the `;`
           remainder = unsafe { remainder.get_unchecked(value_end + 1..) };
           continue;
         }
+        // if we found a space, then insert the tag into the buffer,
+        // and break out of the loop.
         Some(Found::Space(value_end)) => {
           let key = unsafe { leak(remainder.get_unchecked(..key_end)) };
           let value = unsafe { leak(remainder.get_unchecked(value_start..value_end)) };
           whitelist.maybe_insert(&mut tags, key, value);
+          // advance remainder to after the ` `
           remainder = unsafe { remainder.get_unchecked(value_end + 1..) };
           break;
         }
+        // we've somehow found neither. this only happens if the input is malformed.
+        // we treat everything after the `=` as the value, and return an empty remainder.
         None => {
           let key = unsafe { leak(remainder.get_unchecked(..key_end)) };
           let value = unsafe { leak(remainder.get_unchecked(value_start..)) };
@@ -55,6 +79,170 @@ where
   }
 }
 
+/// This function splits `s` into 16-byte chunks, loads each chunk into a 128-bit vector,
+/// and then calls `test` with the vector.
+///
+/// `test` returns:
+/// - nothing, in which case we continue the search
+/// - some position, in which case we end the search and return the position.
+///
+/// If we reach the end of the string without finding a position, then we return nothing as well.
+#[inline]
+fn chunk16_test<T, F>(s: &str, test: F) -> Option<T>
+where
+  T: Add<usize, Output = T>,
+  F: Fn(__m128i) -> Option<T>,
+{
+  let bytes: &[i8] = unsafe { mem::transmute(s.as_bytes()) };
+
+  let mut i = 0usize;
+  while i + 16 <= bytes.len() {
+    // load the data into the vector. this uses an unaligned load because we
+    // cannot guarantee the alignment of the string bytes.
+    let data = unsafe { simd::_mm_loadu_si128(bytes.as_ptr().add(i) as *const _) };
+    if let Some(pos) = test(data) {
+      // `test` returns a position local to its chunk, so we add the total offset
+      // to get the real position.
+      return Some(pos + i);
+    };
+    i += 16;
+  }
+  if i < bytes.len() {
+    // we have less than 16 bytes remaining.
+    // copy the remainder into a 16-byte buffer on the stack.
+    // the buffer can be properly aligned to 16 bytes, so we can
+    // use an aligned load.
+
+    #[repr(align(16))] // force alignment
+    struct Data([i8; 16]);
+
+    let mut buf = Data([0; 16]);
+    buf.0[..bytes.len() - i].copy_from_slice(&bytes[i..]); // memcpy
+
+    // load the data into the vector.
+    let data = unsafe { simd::_mm_load_si128(buf.0.as_ptr() as *const _) };
+    if let Some(pos) = test(data) {
+      // same as above, get the real position.
+      return Some(pos + i);
+    }
+  }
+
+  None
+}
+
+/// Find the first `=` character in `s`.
+///
+/// The implementation splits `s` into 16-byte chunks.
+///
+/// For each chunk, it compares each byte against the `=` character using `cmpeq`.
+/// This produces a 8x16 vector with each 8-bit element set to:
+/// - `0` if the comparison failed
+/// - `255` if the comparison succeeded
+///
+/// `movemask` is applied to the vector, which returns a mask of the MSB of each 8-bit element.
+/// The resulting mask contains:
+/// - `0` if no `=` was found. In this case, we move to the next chunk.
+/// - Some bits set to `1` if the character in that position was `=`.
+///   In this case, we retrieve the position of the character by counting the trailing zeros,
+///   and return it.
+#[inline(always)]
+fn find_equals(s: &str) -> Option<usize> {
+  #[inline(always)]
+  fn test(data: __m128i) -> Option<usize> {
+    // Put `=` in each element of the vector
+    const EQUALS: __m128i = unsafe { mem::transmute([b'=' as i8; 16]) };
+    // compare each element in `data` against `EQUALS`
+    // then use `movemask` to obtain mask which can be later used to obtain the position
+    let mask = unsafe { simd::_mm_movemask_epi8(simd::_mm_cmpeq_epi8(data, EQUALS)) };
+
+    // if the mask is not empty, return the trailing zeros
+    if mask != 0 {
+      Some(mask.trailing_zeros() as usize)
+    } else {
+      None
+    }
+  }
+
+  // execute `test` for each 16-byte chunk of `s`
+  chunk16_test(s, test)
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Found {
+  Semi(usize),
+  Space(usize),
+}
+
+impl Add<usize> for Found {
+  type Output = Self;
+
+  fn add(self, rhs: usize) -> Self::Output {
+    match self {
+      Found::Semi(v) => Found::Semi(v + rhs),
+      Found::Space(v) => Found::Space(v + rhs),
+    }
+  }
+}
+
+impl Add<Found> for usize {
+  type Output = Found;
+
+  fn add(self, rhs: Found) -> Self::Output {
+    match rhs {
+      Found::Semi(v) => Found::Semi(self + v),
+      Found::Space(v) => Found::Space(self + v),
+    }
+  }
+}
+
+/// Find the first `;` or ` ` character in `s`.
+///
+/// If both are present in `s`, the one earlier one will be returned.
+///
+/// This works exactly like `find_equals`, but performs two comparisons at a time
+/// in separate vectors, one for `;` and one for ` `.
+#[inline(always)]
+fn find_semi_or_space(s: &str) -> Option<Found> {
+  #[inline(always)]
+  fn test(data: __m128i) -> Option<Found> {
+    // put `;` in each element of the vector
+    const SEMI: __m128i = unsafe { mem::transmute([b';' as i8; 16]) };
+    // put ` ` in each element of the vector
+    const SPACE: __m128i = unsafe { mem::transmute([b' ' as i8; 16]) };
+
+    // compare each element in `data` against `SEMI`
+    // then use `movemask` to obtain mask which can be later used to obtain the position
+    let semi_mask = unsafe { simd::_mm_movemask_epi8(simd::_mm_cmpeq_epi8(data, SEMI)) };
+    let space_mask = unsafe { simd::_mm_movemask_epi8(simd::_mm_cmpeq_epi8(data, SPACE)) };
+
+    match (semi_mask != 0, space_mask != 0) {
+      (true, true) => {
+        // both characters were found, return the smallest position
+        let semi_tz = semi_mask.trailing_zeros() as usize;
+        let space_tz = space_mask.trailing_zeros() as usize;
+        if semi_tz < space_tz {
+          Some(Found::Semi(semi_tz))
+        } else {
+          Some(Found::Space(space_tz))
+        }
+      }
+      (true, false) => Some(Found::Semi(semi_mask.trailing_zeros() as usize)),
+      (false, true) => Some(Found::Space(space_mask.trailing_zeros() as usize)),
+      _ => None,
+    }
+  }
+
+  // execute `test` for each 16-byte chunk of `s`
+  chunk16_test(s, test)
+}
+
+/// Parse an IRC message prefix:
+///
+/// `:host`
+/// `:nick@host`
+/// `:nick!user@host`
+///
+/// Twitch never sends the `nick@host` form, but we still handle it.
 pub fn parse_prefix(remainder: &str) -> (Option<Prefix<'static>>, &str) {
   if let Some(remainder) = remainder.strip_prefix(':') {
     let bytes: &[i8] = unsafe { mem::transmute(remainder.as_bytes()) };
@@ -133,109 +321,6 @@ pub fn parse_prefix(remainder: &str) -> (Option<Prefix<'static>>, &str) {
   }
 
   (None, remainder)
-}
-
-#[inline]
-fn chunk16_test<T, F>(s: &str, test: F) -> Option<T>
-where
-  T: Add<usize, Output = T>,
-  F: Fn(__m128i) -> Option<T>,
-{
-  let bytes: &[i8] = unsafe { mem::transmute(s.as_bytes()) };
-
-  let mut i = 0usize;
-  while i + 16 <= bytes.len() {
-    let data = unsafe { simd::_mm_loadu_si128(bytes.as_ptr().add(i) as *const _) };
-    if let Some(pos) = test(data) {
-      return Some(pos + i);
-    };
-    i += 16;
-  }
-  if i < bytes.len() {
-    #[repr(align(16))]
-    struct Data([i8; 16]);
-    let mut buf = Data([0; 16]);
-    buf.0[..bytes.len() - i].copy_from_slice(&bytes[i..]);
-    let data = unsafe { simd::_mm_load_si128(buf.0.as_ptr() as *const _) };
-    if let Some(pos) = test(data) {
-      return Some(pos + i);
-    }
-  }
-
-  None
-}
-
-#[inline(always)]
-fn find_equals(s: &str) -> Option<usize> {
-  #[inline(always)]
-  fn test(data: __m128i) -> Option<usize> {
-    const EQUALS: __m128i = unsafe { mem::transmute([b'=' as i8; 16]) };
-    let mask = unsafe { simd::_mm_movemask_epi8(simd::_mm_cmpeq_epi8(data, EQUALS)) };
-    if mask != 0 {
-      Some(mask.trailing_zeros() as usize)
-    } else {
-      None
-    }
-  }
-
-  chunk16_test(s, test)
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-enum Found {
-  Semi(usize),
-  Space(usize),
-}
-
-impl Add<usize> for Found {
-  type Output = Self;
-
-  fn add(self, rhs: usize) -> Self::Output {
-    match self {
-      Found::Semi(v) => Found::Semi(v + rhs),
-      Found::Space(v) => Found::Space(v + rhs),
-    }
-  }
-}
-
-impl Add<Found> for usize {
-  type Output = Found;
-
-  fn add(self, rhs: Found) -> Self::Output {
-    match rhs {
-      Found::Semi(v) => Found::Semi(self + v),
-      Found::Space(v) => Found::Space(self + v),
-    }
-  }
-}
-
-#[inline(always)]
-fn find_semi_or_space(s: &str) -> Option<Found> {
-  #[inline(always)]
-  fn test(data: __m128i) -> Option<Found> {
-    const SEMI: __m128i = unsafe { mem::transmute([b';' as i8; 16]) };
-    const SPACE: __m128i = unsafe { mem::transmute([b' ' as i8; 16]) };
-
-    let semi_mask = unsafe { simd::_mm_movemask_epi8(simd::_mm_cmpeq_epi8(data, SEMI)) };
-    let space_mask = unsafe { simd::_mm_movemask_epi8(simd::_mm_cmpeq_epi8(data, SPACE)) };
-
-    match (semi_mask != 0, space_mask != 0) {
-      (true, true) => {
-        let semi_tz = semi_mask.trailing_zeros() as usize;
-        let space_tz = space_mask.trailing_zeros() as usize;
-        if semi_tz < space_tz {
-          Some(Found::Semi(semi_tz))
-        } else {
-          Some(Found::Space(space_tz))
-        }
-      }
-      (true, false) => Some(Found::Semi(semi_mask.trailing_zeros() as usize)),
-      (false, true) => Some(Found::Space(space_mask.trailing_zeros() as usize)),
-      _ => None,
-    }
-  }
-
-  chunk16_test(s, test)
 }
 
 #[cfg(test)]
