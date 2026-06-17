409d6c066902c55dabd3d1d4a23ad07ab5a365b8
jprochazk <pjanv42@gmail.com>
2026-06-17 11:21:06 +0200

    make benches less noisy

--- benches/parse.rs
+++ benches/parse.rs
@@ -47,9 +47,9 @@ fn twitch(c: &mut Criterion) {
 fn criterion() -> Criterion {
   Criterion::default()
     .configure_from_args()
-    .warm_up_time(Duration::from_millis(100))
-    .measurement_time(Duration::from_secs(1))
-    .sample_size(50)
+    .warm_up_time(Duration::from_secs(2))
+    .measurement_time(Duration::from_secs(10))
+    .sample_size(512)
 }
 
 fn main() {
